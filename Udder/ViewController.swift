936671efd8fe63f84b4dc4f294a8a723290428d5
Tran Chi Thien <thienzone280595@gmail.com>
2020-06-05 22:05:33 +0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by ThienTran on 6/5/20.
+//  Copyright © 2020 ThienTran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
